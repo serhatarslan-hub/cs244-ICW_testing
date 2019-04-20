90e3e9f31f97ffdfa43eb1bb806cbd4de7b0c3ab
Catalin Voss <catalin@cs.stanford.edu>
2019-04-19 19:14:52 -0700

    Refactor test class to use custom exceptions for outputting consistent results + my commentary

--- icw_test.py
+++ icw_test.py
@@ -1,7 +1,7 @@
 from scapy.all import send, sniff, sr, wrpcap  # send, receive, send/receive, and write pcap
 from scapy.all import IP, TCP  # header constructors
 from scapy.all import Padding  # packet layer
-
+import socket  # for capturing bad host errors
 
 class ICWTest(object):
     """
@@ -30,34 +30,66 @@ class ICWTest(object):
             pcap_output: pcap output filename
             rsport: receiver port (don't run multiple tests on the same port simultaneously)
         """
-        syn_ack = self._send_syn(self.url, rsport, mss)
 
-        if syn_ack:
-            if syn_ack.sprintf("%TCP.flags%") == 'SA':
-                responses, request = self._send_request(self.url, syn_ack)
-                icw = self._get_icw(responses, mss)
-                self._send_rst(request)
+        try:
+            # SYN/ACK
+            syn_ack = self._open_connection(self.url, rsport, mss)
 
-                wrpcap(pcap_output, responses)
-                print("** ICW for ", self.url, ": ", icw)
-        else:
-            print("-> Could not get a SYN-ACK response")
+            # Validate ACK
+            if not syn_ack.sprintf("%TCP.flags%") == 'SA':
+                raise ICWTestException(Result.BAD_ACK)
+            self.ip_of_url = syn_ack.src
 
-    def _update_stream(self, packet):
-        self.prev_seqno = self.cur_seqno
-        self.cur_seqno = packet.seq
+            # Perform HTTP request and collect responses
+            responses, request = self._send_request(self.url, syn_ack)
 
-    def _stop_stream(self, packet):
-        FIN = 0x01
-        F = packet['TCP'].flags
+            # Compute ICW
+            icw = self._get_icw(responses, mss)
 
-        if packet['IP'].src != self.ip_of_url or packet.seq < self.prev_seqno or (
-                F & FIN):
-            # Response from a different source,
-            # Retransmission or FIN packet
-            return True
-        else:
-            return False
+            # Close connection using a RST packet and write experiment output
+            self._close_connection(request)
+            wrpcap(pcap_output, responses)
+            print("** ICW for %s: %d" % (self.url, icw))
+
+            return Result.SUCCESS
+
+        except ICWTestException as e:
+            print("Test aborted: %s" % e.message)
+            # Returns one of the Result options defined below
+            return e.message
+
+    def _open_connection(self, url, rsport, mss):
+        """
+        Sends a SYN and waits for the responding SYN/ACK to open the TCP connection.
+        Returns the SYN/ACK response or raises a ICWTestException on error.
+        """
+
+        # Try to send SYN
+        try:
+            syn = IP(dst=url) \
+                  / TCP(sport=rsport, dport=80, flags='S', seq=1, options=[('MSS', mss)])
+        except socket.herror:
+            raise ICWTestException(Result.MALFORMED_HOST)
+        except socket.gaierror:
+            raise ICWTestException(Result.BAD_DNS)
+
+        # Wait for ack
+        ans, _ = sr(syn, timeout=self.ret_timeout)
+        if not ans:
+            # TODO: verify that that is what happened here
+            raise ICWTestException(Result.SYN_ACK_TIMEOUT)
+        
+        return ans[0][1]
+
+    def _close_connection(self, request):
+        """
+        Sends a packet with the RST flag set to close the TCP connection.
+        """
+        rst = IP(dst=request['IP'].dst) \
+              / TCP(dport=80, sport=request.sport, seq=request.seq + len(request['TCP'].payload),
+                    ack=self.prev_seqno + 1, flags='R')
+
+        send(rst)
 
     # TODO: maybe try first the main page, then try this
     def _get_long_str(self):
@@ -67,30 +99,16 @@ class ICWTest(object):
         """
         return 'chicken'*32
 
-    def _send_syn(self, url, rsport, mss):
-        # TODO: wrap this again
-        # try:
-        syn = IP(dst=url) / TCP(sport=rsport, dport=80, flags='S', seq=1,
-                                options=[('MSS', mss)])
-        # except:
-        #     print("-> Could not create the SYN packet")
-        #     return None
-
-        ans, _ = sr(syn, timeout=self.ret_timeout)
-        if ans:
-            self.ip_of_url = ans[0][1].src
-            #print("** ",url," replied from: ",self.ip_of_url)
-            return ans[0][1]
-        else:
-            return None
-
     def _send_request(self, url, syn_ack):
+        """
+        Sends the HTTP request and waits for incoming packets with the provided filters.
+        """
         self.cur_seqno = 0
         self.prev_seqno = 0
         long_str = self._get_long_str()
 
-        get_str = 'GET /' + long_str + ' HTTP/1.1\r\nHost: '
-        get_str += url + '\r\nConnection: Close\r\n\r\n'
+        get_str = 'GET /' + long_str + ' HTTP/1.1\r\nHost: ' \
+                  + url + '\r\nConnection: Close\r\n\r\n'
         get_rqst = IP(dst=syn_ack.src) \
                    / TCP(dport=80, sport=syn_ack.dport, seq=syn_ack.ack, ack=syn_ack.seq + 1,
                          flags='A') \
@@ -104,14 +122,32 @@ class ICWTest(object):
 
         return packets, get_rqst
 
-    def _send_rst(self, request):
-        rst = IP(dst=request['IP'].dst) \
-              / TCP(dport=80, sport=request.sport, seq=request.seq + len(request['TCP'].payload),
-                    ack=self.prev_seqno + 1, flags='R')
+    def _update_stream(self, packet):
+        """
+        Update state helper for _send_request.
+        """
+        self.prev_seqno = self.cur_seqno
+        self.cur_seqno = packet.seq
 
-        send(rst)
+    def _stop_stream(self, packet):
+        """
+        Stop packet filter for _send_request.
+        """
+        FIN = 0x01
+        F = packet['TCP'].flags
+
+        if packet['IP'].src != self.ip_of_url or packet.seq < self.prev_seqno or (
+                F & FIN):
+            # Response from a different source,
+            # Retransmission or FIN packet
+            return True
+        else:
+            return False
 
     def _get_icw(self, responses, mss):
+        """
+        Computes the initial congestion window from the provided packet stream.
+        """
         seen_seqno = 0
         icw = 0
         FIN = 0x01
@@ -140,3 +176,23 @@ class ICWTest(object):
                     icw += 1
         return icw
 
+
+class Result(object):
+    # User wrote bad host
+    MALFORMED_HOST = "malformed_host"
+
+    # DNS lookup error (old host?)
+    BAD_DNS = "dns"
+
+    BAD_ACK = "bad_ack"
+
+    SYN_ACK_TIMEOUT = "ack_timeout"
+
+    SUCCESS = "success"
+
+
+class ICWTestException(Exception):
+    """
+    We keep track of our own exceptions
+    """
+    pass
