023744103adc1d95a199614045e3539827ecb9cf
Catalin Voss <catalin@cs.stanford.edu>
2019-04-20 16:57:25 -0700

    Loop over ports from 2048 to 65500 in a random order

--- run_icw_test.py
+++ run_icw_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from argparse import ArgumentParser
 from icw_test import ICWTest, Result
-import random
+import numpy as np
 
 
 def read_url_list(filename):
@@ -15,18 +15,18 @@ def main():
                         help="File that contains the list of URLs to measure.")
     args = parser.parse_args()
 
-    url_list = read_url_list(args.url_list)
-    print("Performing ICW test on %d URLs." % len(url_list))
+    urls = read_url_list(args.url_list)
+    print("Performing ICW test on %d URLs." % len(urls))
 
     # Some linux servers will automatically make it 64 per min, but 48 is safe
     mss = 48
 
-    for url in url_list:
+    # Loop over ports from 2048 to 65500 in a random order
+    ports = np.random.permutation(np.arange(2048, 65500))
+
+    for url, rsport in zip(urls, ports):
         print("="*32)
-        print("Testing: %s" % url)
-        # TODO: loop over ports insted
-        # Start from 65k and go down
-        rsport = random.randrange(2048, 65500)
+        print("Testing: %s on port %d" % (url, rsport))
 
         experiment = ICWTest(url=url)
         result, icw = experiment.run_test(
